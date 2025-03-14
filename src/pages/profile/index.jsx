eb80c293a87d7e8ffd75c719d8b41f8882c41ef0
NotlimitGB <nik.ryabov.05@mail.ru>
2025-03-14 13:42:17 +0300

    popup-profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -11,22 +11,32 @@ export default function ProfilePage() {
 
   const [mainImage, setMainImage] = useState();
 
-  useEffect(() => {    
+  useEffect(() => {
     if (models_ID?.data?.images) {
       setMainImage(`http://localhost:3000${models_ID?.data?.images[0]}`);
     }
   }, [models_ID?.data]);
 
-  const handleThumbnailClick = (image) => {    
+  const handleThumbnailClick = (image) => {
     setMainImage(image);
   };
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.querySelector(".profile-cont").style.filter = "blur(5px)";
+  }
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.querySelector(".profile-cont").style.filter = "blur(0)";
+
+  }
 
   return (
     <>
       <section>
         <div className="profile-cont">
           <div className="profile-image">
-              <img src={mainImage} alt="" />
+            <img src={mainImage} alt="" />
             <div className="thumbnails">
               {models_ID?.data?.images &&
                 models_ID?.data?.images.length &&
@@ -46,8 +56,24 @@ export default function ProfilePage() {
           <div className="profile-info">
             <h2>{models_ID?.data?.name}</h2>
             <p>{models_ID?.data?.description}</p>
-            <button className="edit-btn">Связаться</button>
+            <button className="edit-btn" onClick={() => openForm()}>
+              Связаться
+            </button>
           </div>
+        </div>
+        <div className="form-popup" id="myForm">
+          <form action="" method="POST">
+            <label htmlFor="name">Имя:</label>
+            <input type="text" id="name" name="name" required />
+            <label htmlFor="phone">Телефон:</label>
+            <input type="tel" id="phone" name="phone" required />
+            <label htmlFor="text">Обращене</label>
+            <input type="text" />
+            <input type="submit" value="Отправить" />
+          </form>
+          <button type="button" className="close" onClick={() => closeForm()}>
+            &times;
+          </button>
         </div>
       </section>
     </>

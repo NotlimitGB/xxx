108ccbe62fe7a1a55b8dab1c4ea29a0c7276986b
NotlimitGB <nik.ryabov.05@mail.ru>
2025-10-04 14:22:36 +0300

    vite_fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
-  }
-})
+    host: '0.0.0.0', // важно для Docker
+    port: 5173, // порт, который ты expose в Docker
+    hmr: {
+      host: 'localhost', // для WebSocket HMR
+      protocol: 'wss', // https через Nginx
+    },
+  },
+});

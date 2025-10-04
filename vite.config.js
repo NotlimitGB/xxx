43f633415fe56747ba10075e3c6c7a62cb1a7f8b
NotlimitGB <nik.ryabov.05@mail.ru>
2025-10-04 14:34:43 +0300

    pwe

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: '0.0.0.0', // важно для Docker
     port: 5173, // порт, который ты expose в Docker
     hmr: {
-      host: 'localhost', // для WebSocket HMR
+      host: '10.0.2.15', // для WebSocket HMR
       protocol: 'wss', // https через Nginx
     },
   },
